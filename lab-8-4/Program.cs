c11a1dc1f5a6eadd1de152c1efe2a14f5766e897
Денис Гура <denis777gura@gmail.com>
2019-05-28 12:46:09 +0300

    Добавьте файлы проекта.

---
+++ lab-8-4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace prog_4s_8_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before");
+            int[] arr = System.IO.File.ReadAllText("in.txt").Split(' ').Select(n => int.Parse(n)).ToArray();
+            Stack<int> numbers = new Stack<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                numbers.Push(arr[i]);
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine("after");
+            StreamWriter sw = new StreamWriter("out.txt");
+            while (numbers.Count != 0)
+            {
+                int stackElement = numbers.Pop();
+                sw.Write(stackElement);
+                Console.WriteLine(stackElement);
+
+            }
+            sw.Close();
+
+        }
+    }
+}
